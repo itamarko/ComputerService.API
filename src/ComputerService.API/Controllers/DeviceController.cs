0fc90fbc831304f6d34a333799615c1691d6cfbb
itamarko <marko.stojicevic@it-akademija.com>
2023-07-17 19:48:52 +0200

    Dodat kontroler

---
+++ src/ComputerService.API/Controllers/DeviceController.cs
@@ -0,0 +1,36 @@
+﻿using ComputerService.BusinessLogic.Interfaces;
+using ComputerService.BusinessModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ComputerService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        private readonly IDeviceBL _deviceBL;
+
+        public DeviceController(IDeviceBL deviceBL)
+        {
+            _deviceBL = deviceBL;
+        }
+
+        [HttpPost]
+        public ActionResult<CommandResponse<AddItemResponse>> Post(Item item)
+        {
+            CommandResponse<AddItemResponse> result = new CommandResponse<AddItemResponse>();
+            try
+            {
+                result = _deviceBL.AddItem(item);
+            }
+            catch (Exception ex) 
+            {
+                result.Success = false;
+                result.ErrorMessage = ex.Message;   
+            }
+
+            return Ok(result);
+        }
+    }
+}
